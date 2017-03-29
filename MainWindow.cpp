b11896dc32e1d5cddd22fd78c887bd1cee1ccc8a
ArRana <xholycockx@gmail.com>
2017-03-29 17:36:12 +0300

    Fix Morse code

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -117,42 +117,42 @@ void MainWindow::on_saveResBtn_clicked()
 QString engToMorse(QString str)
 {
     QMap <QString, QString> toMorse; // create map to contain keys / values
-    toMorse["A"] = ". -";
-    toMorse["B"] = "- . . .";
-    toMorse["C"] = "- . - .";
-    toMorse["D"] = "- . .";
+    toMorse["A"] = ".-";
+    toMorse["B"] = "-...";
+    toMorse["C"] = "-.-.";
+    toMorse["D"] = "-..";
     toMorse["E"] = ".";
-    toMorse["F"] = ". . - .";
-    toMorse["G"] = "- - .";
-    toMorse["H"] = ". . . .";
-    toMorse["I"] = ". .";
-    toMorse["J"] = ". - - -";
-    toMorse["K"] = "- . -";
-    toMorse["L"] = ". - . .";
-    toMorse["M"] = "- -";
-    toMorse["N"] = "- .";
-    toMorse["O"] = "- - -";
-    toMorse["P"] = ". - - .";
-    toMorse["Q"] = "- - . -";
-    toMorse["R"] = ". - .";
-    toMorse["S"] = ". . .";
+    toMorse["F"] = "..-.";
+    toMorse["G"] = "--.";
+    toMorse["H"] = "....";
+    toMorse["I"] = "..";
+    toMorse["J"] = ".---";
+    toMorse["K"] = "-.-";
+    toMorse["L"] = ".-..";
+    toMorse["M"] = "--";
+    toMorse["N"] = "-.";
+    toMorse["O"] = "---";
+    toMorse["P"] = ".--.";
+    toMorse["Q"] = "--.-";
+    toMorse["R"] = ".-.";
+    toMorse["S"] = "...";
     toMorse["T"] = "-";
-    toMorse["U"] = ". . -";
-    toMorse["V"] = ". . . -";
-    toMorse["W"] = ". - -";
-    toMorse["X"] = "- . . -";
-    toMorse["Y"] = "- . - -";
-    toMorse["Z"] = "- - . .";
-    toMorse["1"] = ". - - - -";
-    toMorse["2"] = ". . - - -";
-    toMorse["3"] = ". . .- -";
-    toMorse["4"] = ". . . . -";
-    toMorse["5"] = ". . . . .";
-    toMorse["6"] = "- . . . .";
-    toMorse["7"] = "- - . . .";
-    toMorse["8"] = "- - - . .";
-    toMorse["9"] = "- - - - .";
-    toMorse["0"] = "- - - - -";
+    toMorse["U"] = "..-";
+    toMorse["V"] = "...-";
+    toMorse["W"] = ".--";
+    toMorse["X"] = "-..-";
+    toMorse["Y"] = "-.--";
+    toMorse["Z"] = "--..";
+    toMorse["1"] = ".----";
+    toMorse["2"] = "..---";
+    toMorse["3"] = "...--";
+    toMorse["4"] = "....-";
+    toMorse["5"] = ".....";
+    toMorse["6"] = "-....";
+    toMorse["7"] = "--...";
+    toMorse["8"] = "---..";
+    toMorse["9"] = "----.";
+    toMorse["0"] = "-----";
 
     QMap <QString, QString>::const_iterator it;
 
@@ -183,42 +183,49 @@ QString engToMorse(QString str)
 QString morseToEng (QString str)
 {
     QMap <QString, QString> toEnglish; // create map to contain keys / values
-    toEnglish[". -"] = "A";
-    toEnglish["- . . ."] = "B";
-    toEnglish["- . - ."] = "C";
-    toEnglish["- . ."] = "D";
+    toEnglish[".-"] = "A";
+    toEnglish["-..."] = "B";
+    toEnglish["-.-."] = "C";
+    toEnglish["-.."] = "D";
     toEnglish["."] = "E";
-    toEnglish[". . - ."] = "F";
-    toEnglish["- - ."] = "G";
-    toEnglish[". . . ."] = "H";
-    toEnglish[". ."] = "I";
-    toEnglish[". - - -"] = "J";
-    toEnglish["- . -"] = "K";
-    toEnglish[". - . ."] = "L";
-    toEnglish["- -"] = "M";
-    toEnglish["- ."] = "N";
-    toEnglish["- - -"] = "O";
-    toEnglish[". - - ."] = "P";
-    toEnglish["- - . -"] = "Q";
-    toEnglish[". - ."] = "R";
-    toEnglish[". . ."] = "S";
+    toEnglish["..-."] = "F";
+    toEnglish["--."] = "G";
+    toEnglish["...."] = "H";
+    toEnglish[".."] = "I";
+    toEnglish[".---"] = "J";
+    toEnglish["-.-"] = "K";
+    toEnglish[".-.."] = "L";
+    toEnglish["--"] = "M";
+    toEnglish["-."] = "N";
+    toEnglish["---"] = "O";
+    toEnglish[".--."] = "P";
+    toEnglish["--.-"] = "Q";
+    toEnglish[".-."] = "R";
+    toEnglish["..."] = "S";
     toEnglish["-"] = "T";
-    toEnglish[". . -"] = "U";
-    toEnglish[". . . -"] = "V";
-    toEnglish[". - -"] = "W";
-    toEnglish["- . . -"] = "X";
-    toEnglish["- . - -"] = "Y";
-    toEnglish["- - . ."] = "Z";
-    toEnglish[". - - - -"] = "1";
-    toEnglish[". . - - -"] = "2";
-    toEnglish[". . .- -"] = "3";
-    toEnglish[". . . . -"] = "4";
-    toEnglish[". . . . ."] = "5";
-    toEnglish["- . . . ."] = "6";
-    toEnglish["- - . . ."] = "7";
-    toEnglish["- - - . ."] = "8";
-    toEnglish["- - - - ."] = "9";
-    toEnglish["- - - - -"] = "0";
+    toEnglish["..-"] = "U";
+    toEnglish["...-"] = "V";
+    toEnglish[".--"] = "W";
+    toEnglish["-..-"] = "X";
+    toEnglish["-.--"] = "Y";
+    toEnglish["--.."] = "Z";
+    toEnglish[".----"] = "1";
+    toEnglish["..---"] = "2";
+    toEnglish["...--"] = "3";
+    toEnglish["....-"] = "4";
+    toEnglish["....."] = "5";
+    toEnglish["-...."] = "6";
+    toEnglish["--..."] = "7";
+    toEnglish["---.."] = "8";
+    toEnglish["----."] = "9";
+    toEnglish["-----"] = "0";
+
+    QMap <QString, QString>::const_iterator it; // iterate over a QMap
+    QString result; // to keep out result
+
+    //
+
+    return result;
 }
 
 
